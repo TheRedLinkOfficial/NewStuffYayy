da06341dcfe84a67d524f65cbefe8700ac7baa1e
Pietro Dieso de Oliveira Galvão <pietro.dieso@escolar.ifrn.edu.br>
2026-03-31 15:40:12 -0300

    idkhowtonameit

--- NewThingYeah/Assets/SourceFiles/Scripts/AudioManager.cs
+++ NewThingYeah/Assets/SourceFiles/Scripts/AudioManager.cs
@@ -7,6 +7,7 @@ public class AudioManager : MonoBehaviour
     private AudioSource systemsource;
     private List<AudioSource> activeSources;
 
+    #region awake
     private void Awake()
     {
         if (instance == null)
@@ -20,6 +21,41 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    #endregion awake
+    
+    
+    #region audioplayback
+
+    public void play(AudioClip clip)
+    {
+        systemsource.Stop();
+        systemsource.clip = clip;
+        systemsource.Play();
+    }
+
+    public void playoneshot(AudioClip clip)
+    {
+        systemsource.PlayOneShot(clip);
+    }
+
+    public void stop()
+    {
+        systemsource.Stop();
+    }
+
+    public void pause()
+    {
+        systemsource.Pause();
+    }
+
+    public void resume()
+    {
+        systemsource.UnPause();
+    }
+        
+        
+        #endregion audioplayback
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
